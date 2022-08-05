6528a1ea756cb6f49bc9ebebb02aaf71d1e7ba2b
vinsinned <dagger1245653@gmail.com>
2022-08-05 11:38:24 -0700

    Add profile and responsiveness to navbar.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -46,7 +46,7 @@ export default function Navbar() {
 							notifications
 						</span>
 						<div className="profile">
-
+							<img className="profile__image" src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" />
 						</div>
 					</div>
 				</nav>
